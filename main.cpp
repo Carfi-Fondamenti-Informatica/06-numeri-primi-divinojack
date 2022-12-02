c446c438503a0e091f90129c957bbb6fce68d47a
divinojack <115471906+divinojack@users.noreply.github.com>
2022-12-02 18:50:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+usingnamespace std;
 
 int main(){
+  int h;
+    cin>>h;
+    if (numeroprimo(h)==0){
+        cout<<"numero primo"<< endl;
+    } else{
+        cout<< "numero non primo"<< endl;
+    }
   return 0;
 }
